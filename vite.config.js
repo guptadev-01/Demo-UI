9f8759cac82d5aff5822cbb9551b42d124bce8a9
guptadev-01 <devgupta1105@gmail.com>
2025-05-31 16:08:36 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import dns from 'node:dns'
 
-// https://vite.dev/config/
+dns.setDefaultResultOrder('verbatim')
+
 export default defineConfig({
-  plugins: [react()],
+  // omit
 })
